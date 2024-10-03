9bf72c5ca969b478e3553bbfbed62fc5c356cc80
behruz171 <behruzzo662@gmail.com>
2024-10-03 07:30:03 +0500

    nimadir

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!','Munisa Holajon' ],
+			strings: [ 'HAPPY', 'BIRTHDAY!','Holajon' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
