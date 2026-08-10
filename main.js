1492a0ce8b4421670e61229fd578c14a3f0f5c40
behruz171 <120024435+behruz171@users.noreply.github.com>
2026-08-10 15:40:28 +0500

    Update birthday strings in main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!','Munisa' ,'Holajon❤' ],
+			strings: [ "Tug'ilgan", 'kuniz','bilan!' ,'Shahnoza❤', "kenayi" ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
